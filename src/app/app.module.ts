1b847c0bc6a62ce05d90a83eda328788268b8ec3
chadirahme <eng.chadi@gmail.com>
2021-09-04 00:41:57 -0400

    add toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { ResponsiveToolbarComponent } from './responsive-toolbar/responsive-toolbar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResponsiveToolbarComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
     AppRoutingModule
   ],
   providers: [],
